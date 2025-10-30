2edb563c88b78c805d2a7adcd776a31099d7f618
sam <me@sammason.dev>
2025-10-30 16:04:12 -0400

    moved queries into queries.py

--- database.py
+++ database.py
@@ -1,24 +1,26 @@
 from datetime import date
 import sqlite3
 
+from queries import (
+    create_table_entries,
+    insert_into_entries,
+    select_content_and_date_from_entries,
+)
+
 connection = sqlite3.connect("data.db")
 connection.row_factory = sqlite3.Row
 
 
 def create_table():
     with connection:
-        connection.execute(
-            "CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);"
-        )
+        connection.execute(create_table_entries)
 
 
 def add_entry(entry_content: str, entry_date: date):
     with connection:
-        connection.execute(
-            "INSERT INTO entries VALUES (?, ?);", (entry_content, str(entry_date))
-        )
+        connection.execute(insert_into_entries, (entry_content, entry_date))
 
 
 def get_entries() -> sqlite3.Cursor:
-    cursor = connection.execute("SELECT content, date FROM entries;")
+    cursor = connection.execute(select_content_and_date_from_entries)
     return cursor
---
+++ queries.py
@@ -0,0 +1,5 @@
+select_content_and_date_from_entries = "SELECT content, date FROM entries;"
+
+insert_into_entries = "INSERT INTO entries VALUES (?, ?);"
+
+create_table_entries = "CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);"
